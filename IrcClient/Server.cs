041e475a5f67e8a73ab1540ca7acd7865ec033da
Katsuhiro Hermit Endo <hermit@koka-in.org>
2009-05-08 08:52:02 +0900

    Import.

---
+++ IrcClient/Server.cs
@@ -0,0 +1,17 @@
+﻿
+namespace IrcClient
+{
+  /// <summary>
+  /// IRCクライアントアプリケーションから、接続するサーバを扱うためのクラス
+  /// </summary>
+  public class Server
+  {
+    public string Name { get; set; }
+
+    public string Host { get; set; }
+    public ushort Port { get; set; }
+
+    public string Nickname { get; set; }
+    public string Password { get; set; }
+  }
+}
